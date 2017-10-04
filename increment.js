0e2b141cd1b309bac347e9999962fdaa68b8f868
Caitlin Curbow <cc0722665@otc.edu>
2017-10-04 13:22:23 -0500

    workin on it

--- increment.js
+++ increment.js
@@ -1,9 +1,17 @@
-let buttons = document.getElementsByClassName("increment")
-buttons = Array.from(buttons)
-buttons.forEach(button =>{
-    button.addEventListener("click", e =>{
-        let currentCount = button.nextSibling.textContent//.trim
-        newCount = parseInt(currentCount) + 1
-        button.nextSibling.textContent = newCount
+$(function(){
+    $('.glyphicon glyphicon-heart').click(({target}) => {
+        console.log(target)
+        let currentCount = $(target).children('div')[0]
+        console.log(currentCount)
     })
 })
+
+// let buttons = document.getElementsByClassName("increment")
+// buttons = Array.from(buttons)
+// buttons.forEach(button =>{
+//     button.addEventListener("click", e =>{
+//         let currentCount = button.nextSibling.textContent//.trim
+//         newCount = parseInt(currentCount) + 1
+//         button.nextSibling.textContent = newCount
+//     })
+// })
