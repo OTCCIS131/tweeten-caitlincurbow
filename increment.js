96ee6ad178e7cdfce9e80e462a5c214bf567476a
Caitlin Curbow <cc0722665@otc.edu>
2017-10-14 21:50:44 -0500

    tweet button

--- increment.js
+++ increment.js
@@ -1,9 +1,26 @@
 $(function(){
-    $('.glyphicon glyphicon-heart').click(({target}) => {
-        console.log(target)
-        let currentCount = $(target).children('div')[0]
-        console.log(currentCount)
+    // $('.glyphicon glyphicon-heart').click(({target}) => {
+    //     console.log(target)
+    //     let currentCount = $(target).children('div')[0]
+    //     console.log(currentCount)
+    // })
+
+    $("#newTweetForm").submit(function (e) {
+        e.preventDefault()
+        let newTweetInput = $("[tweet=title]", $(this))
+        let newTweet = newTweetInput.val()
+
+        if (newTweet.trim() == '') return
+        
+        let storeTweet = $("li:last-child". $("#tweetList")).clone()
+
+        storeTweet.text(newTweet)
+        
+        $("#tweetList").clone().appendTo(storeTweet)
+        addDeleteButton(storeTweet)
+        newTweetInput.val('')
     })
+
 })
 
 // let buttons = document.getElementsByClassName("increment")
